9864a2f3793a39ad276494e062a509d62154eafc
kakisoft <kakisttab@gmail.com>
2018-01-03 03:43:08 +0900

    edit

---
+++ MyPHP03.php
@@ -0,0 +1,11 @@
+<?php
+
+//==========================
+//        Cookie
+//==========================
+setcookie("username", "taguchi"); // 第三引数にて、有効期限を設定できる。設定しない場合、ブラウザを閉じるまで有効。
+// setcookie("username", "taguchi", time()+60*60); // 有効期限：1時間
+
+// setcookie("username", "taguchi", time()-60*60); // Cookieを削除する場合、適当なマイナス値を設定する。
+
+echo $_COOKIE['username'];
