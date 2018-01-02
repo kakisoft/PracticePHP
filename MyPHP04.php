d8286d5bdf2cd61e50d9c9d1e12d5640511e3724
kakisoft <kakisttab@gmail.com>
2018-01-03 03:48:43 +0900

    edit

---
+++ MyPHP04.php
@@ -0,0 +1,15 @@
+<?php
+
+//==========================
+//        session
+// 複数ページにまたがって設定できる値（サーバ側に保持）
+//==========================
+
+session_start(); //セッション開始
+
+$_SESSION['username'] = "sawai";
+
+echo $_SESSION['username'];
+
+// unset($_SESSION['username']); //セッション終了
+
