e001ca4d92318e55c1d61ab5222674dcfa0045d1
kakisoft <kakisttab@gmail.com>
2018-08-23 23:21:25 +0900

    e

---
+++ Smarty/Sample01/app/sample01.php
@@ -0,0 +1,13 @@
+<?php
+require '../libs/Smarty.class.php';
+
+$smarty = new Smarty;
+
+$smarty->assign("Name"   , "kakisoft", true);
+$smarty->assign("message", "msg01"   , true);
+$smarty->assign("contacts", array(array("phone" => "1", "fax" => "2", "cell" => "3"),
+    array("phone" => "555-4444", "fax" => "555-3333", "cell" => "760-1234"))
+);
+
+$smarty->display('sample01.tpl');
+
---
+++ Smarty/Sample01/app/sample01.tpl
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Smarty Test</title>
+</head>
+<body>
+<p>$smarty->assign("Name"   , "kakisoft", true);    ：{$Name}</p>
+<p>$smarty->assign("message", "msg01"   , true);    ：{$message}</p>
+
+
+</body>
+</html>
