76cc240da0cf9a9463649bf5a3b57cb676377564
kakisoft <kakisttab@gmail.com>
2018-09-15 02:25:57 +0900

    e

--- 06_twitter_bot/bot.php
+++ 06_twitter_bot/bot.php
@@ -1,3 +1,17 @@
 <?php
 
 require_once(__DIR__ . '/config.php');
+
+// package
+// - Composer
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$connection = new TwitterOAuth(
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_SECRET);
+$content = $connection->get("account/verify_credentials");
+
+var_dump($content);
---
+++ 06_twitter_bot/config.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once(__DIR__ . '/vendor/autoload.php');
+
+define('CONSUMER_KEY', '******************************');
+define('CONSUMER_SECRET', '******************************');
+define('ACCESS_TOKEN', '******************************');
+define('ACCESS_TOKEN_SECRET', '******************************');
