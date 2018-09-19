7348476e4f69a586b2d281f876736a62629b2009
kakisoft <kakisttab@gmail.com>
2018-09-19 09:58:38 +0900

    e

---
+++ 00_Sample01/APISample04.php
@@ -0,0 +1,19 @@
+<?php
+$context = stream_context_create(
+    array(
+        'http' => array(
+            'method'=> 'POST',
+            'header'=> 'Content-type: application/json; charset=UTF-8',
+            'content' => json_encode(
+                array(
+                    'title' => 'file_get_contents で POST',
+                    'raw' => "file_get_contents で POST\nPHP すごい...\n"
+                )
+            )
+        )
+    )
+);
+
+$content = file_get_contents('http://challenge-your-limits.herokuapp.com/call/me', false, $context);
+
+var_dump($content);
