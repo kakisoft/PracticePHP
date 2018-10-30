d96168d5da03c1e75cc4a57411a42bf0c93142ab
kakisoft <kakisttab@gmail.com>
2018-10-30 18:11:39 +0900

    e

--- 80_parts/Format.php
+++ 80_parts/Format.php
@@ -5,7 +5,6 @@
 	 */
 	public static function sharpen($str) {
 		// htmlspecialchars($str, ENT_QUOTES);
-		$str = MSM_Utils_Text::escHtml($str);
 		$str = preg_replace('#\r?\n#u', ' ', $str);
 		$str = preg_replace('#\t#u', ' ', $str);
 		$str = preg_replace('#^[ 　]+#u', '', $str);
