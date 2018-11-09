cf18bf1292b57f99193bb9440a6ba49936751df0
kakisoft <kakisttab@gmail.com>
2018-11-10 02:38:12 +0900

    e

--- MyPHP91.php
+++ MyPHP91.php
@@ -4,4 +4,8 @@
 if (strpos($class, $prefix) === 0) {
 }
 
+//URL:
+$requestUri = parse_url($_SERVER["REQUEST_URI"]);
+$requestQuery = $requestUri["query"];
+$this->requestQuery = preg_replace('/\?dnumber=[0-9]*&sort=[0-9]*&_page=[0-9]/', "", $requestQuery);
 
