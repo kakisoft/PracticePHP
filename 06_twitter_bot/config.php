96c01c4c4a3234dc1f5b12844bd89dacdae64a2b
kakisoft <kakisttab@gmail.com>
2020-02-27 08:02:14 +0900

    a

--- 06_twitter_bot/config.php
+++ 06_twitter_bot/config.php
@@ -7,7 +7,3 @@ require_once(__DIR__ . '/vendor/autoload.php');
 // define('ACCESS_TOKEN'       , '****');
 // define('ACCESS_TOKEN_SECRET', '****');
 
-define('API_KEY'            , 'cgeN1Xk5aYiEaJ1NfCCHKQ73L');
-define('API_SECRET_KEY'     , 'ZE3zcvbrfll84nQu1ALqZiD4tsRbek5vZoXwnryUdVQbvw72Fz');
-define('ACCESS_TOKEN'       , '1040188483266994176-ktH9EuEWQlKriIcq75zQC0LWqKvp4t');
-define('ACCESS_TOKEN_SECRET', 'WZ0faoINKLupla8t62w2Jl7GLBlMG5lOrywodmeV21WOx');
