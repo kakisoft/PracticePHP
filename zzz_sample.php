438fa7b1397e5b175a48c951d7d735271fff2345
kakisoft <kakisttab@gmail.com>
2020-04-12 01:12:51 +0900

    x

--- zzz_sample.php
+++ zzz_sample.php
@@ -1,21 +1,3 @@
 <?php
 
 
-// list($array_01, $array_02, $array_03) = getListData();
-[$array_01, $array_02, $array_03] = getListData();
-
-function getListData(){
-    $array_01 = [1, 3, 5];
-    $array_02 = ['A', 'C', 'D'];
-    $array_03 = ['n', 'm', 'k'];
-
-    return array($array_01, $array_02, $array_03);
-}
-
-
-print_r($array_01);
-print_r($array_02);
-print_r($array_03);
-
-
-
