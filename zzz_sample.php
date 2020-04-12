f2355a18e26a7b335578d66f3779f23eca2e013a
kakisoft <kakisttab@gmail.com>
2020-04-12 12:25:35 +0900

    s

--- zzz_sample.php
+++ zzz_sample.php
@@ -1,9 +1,13 @@
 <?php
 
+// array_splice — 配列の一部を削除し、他の要素で置換する
+$scores_1 = [30, 40, 50, 60, 70, 80];
+$scores_2 = [30, 40, 50, 60, 70, 80];
+$scores_3 = [30, 40, 50, 60, 70, 80];
 
-$input = array("a", "b", "c", "d", "e");
+array_splice($scores_1, 2, 3);              //=> Array( 30, 40, 80)
+array_splice($scores_2, 2, 3, 100);         //=> Array( 30, 40, 100, 80)
+array_splice($scores_3, 2, 0, [100, 101]);  //=> Array( 30, 40, 100, 101, 50, 60, 70, 80)
 
-$output_01 = array_slice($input, -2);      // returns d", "e"
+print_r($scores_3);
 
-
-print_r($output_01);
