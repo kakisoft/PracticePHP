95076a96c2b26b89b51c2c03ae2edd0cb01d6dba
satoru_kakinohana <satoru_kakinohana@bwave.co.jp>
2020-11-12 21:18:03 +0900

    f

--- zzz_sample.php
+++ zzz_sample.php
@@ -2,5 +2,12 @@
 
 
 
-print vsprintf("%04d-%02d-%02d", explode('-', '1988-8-1'));
-//=> 1988-08-01
+// print vsprintf("%04d-%02d-%02d", explode('-', '1988-8-1'));
+// //=> 1988-08-01
+
+
+$str = "Is your name O'Reilly?";
+
+// 出力: Is your name O\'Reilly?
+echo addslashes($str);
+
