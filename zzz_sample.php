ce3eb0072af9315f2d2ca3dce9ad2d0e61bbdeea
satoru_kakinohana <satoru_kakinohana@bwave.co.jp>
2021-04-06 00:21:10 +0900

    a

--- zzz_sample.php
+++ zzz_sample.php
@@ -1,22 +1,55 @@
 <?php
 
-// 2
+// use Exception;
 
-// print vsprintf("%04d-%02d-%02d", explode('-', '1988-8-1'));
-// //=> 1988-08-01
+function div27($a, $b) {
+  try {
+      if ($b === 0) {
+          throw new Exception("cannot divide by 0!");
+      }
+      echo $a / $b;
+      echo PHP_EOL;
+
+  } catch (Exception $e) {
+      // エラーメッセージを取得する
+      echo $e->getMessage();
+      echo PHP_EOL . PHP_EOL;
+
+      // Exception::getCode
+      // 例外コードを取得する
+      echo "The exception code is : " . $e->getCode ();  //=> 例外コードを取得
+      echo PHP_EOL . PHP_EOL;
+
+      // Exception::getFile
+      // 例外が作られたファイルを取得する
+      echo "The exception was created in : " . $e->getFile();  //=> 例外が発生したファイルを表示
+      echo PHP_EOL . PHP_EOL;
+
+      // Exception::getLine
+      // 例外が作られた行を取得する
+      echo "The exception was created on line: " . $e->getLine();  //=> 例外が発生した行数を表示
+      echo PHP_EOL . PHP_EOL;
+
+      // Exception::getTrace
+      // スタックトレースを取得する
+      print_r($e->getTrace());  //=> 配列形式
+      echo PHP_EOL . PHP_EOL;
+
+      // Exception::getTraceAsString
+      // スタックトレースを文字列で取得する
+      echo "The exception's stack trace: " . $e->getTraceAsString ();  //=> スタックトレース
+      echo PHP_EOL . PHP_EOL;
 
 
-$str = "Is your name O'Reilly?";
-$a   = 111;
-$a   = 111;
-$a   = 111;
-function aaa(){
-    $a1 = 111;
-        $a   = 111;
-        	$b    = 333;
-				return $b;
+  } finally {
+      echo "First finally." . PHP_EOL;
+  }
 }
 
-// 出力: Is your name O\'Reilly?
-echo addslashes($str);
+div27(7, 2);
+div27(5, 0);
+
+
+
+
 
