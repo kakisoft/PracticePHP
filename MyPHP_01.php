28ee01a27ee6b7b644a1834b826768570507dbf1
satoru_kakinohana <satoru_kakinohana@bwave.co.jp>
2021-08-07 11:25:18 +0900

    a

--- MyPHP_01.php
+++ MyPHP_01.php
@@ -3665,7 +3665,11 @@ class SampleClass01
 $target_class = new SampleClass01();
 
 $target_class_name = get_class($target_class);
-echo "{$target_class_name}"; //=> SampleClass01
+echo "{$target_class_name}" . PHP_EOL; //=> SampleClass01
+
+//-----( クラスから )-----
+$target_class_name = strval(SampleClass01::class);
+echo "{$target_class_name}" . PHP_EOL; //=> SampleClass01
 
 
 //====================================
--- zzz_sample.php
+++ zzz_sample.php
@@ -1,28 +1,16 @@
 <?php
 
-$format = 'Y-m-d H:i:s';
-// $format = 'Y-m-d';
+class SampleClass01
+{
 
-// 2021-08-06  05:54:28
-$dateTime = "2021-08-06  06:26:55";
-// $dateTime = "2021-08-06";
+}
+$target_class = new SampleClass01();
 
+$target_class_name = get_class($target_class);
+echo "{$target_class_name}" . PHP_EOL; //=> SampleClass01
 
-$d = DateTime::createFromFormat($format, $dateTime);
-
-// var_dump($d);
-
-// $d2 = DateTime::createFromFormat('Y-m-d', "2021-08-06");
-$d2 = DateTime::createFromFormat('Y-m-d H:i:s', "2021
--08-06  06:26:55");
-$d3 = DateTime::createFromFormat('YmdHis', "20210806062655");
-
-
-// var_dump($d2);
-var_dump($d3);
-
-
-return $d && $d->format($format) == $dateTime;
-
+//-----( クラスから )-----
+$target_class_name = strval(SampleClass01::class);
+echo "{$target_class_name}" . PHP_EOL; //=> SampleClass01
 
 
