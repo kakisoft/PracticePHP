363f9986ab0bf9a19a4e03938b355a3674c5944d
satoru_kakinohana <satoru_kakinohana@bwave.co.jp>
2021-08-07 12:43:12 +0900

    a

--- Laravel/aaaaaaaaaaa/blog02.md
+++ Laravel/aaaaaaaaaaa/blog02.md
@@ -1,3 +1,21 @@
 
 
+```php
+//-----[ 文字列の置換：配列で対応内容を指定（strtr と str_replace で差が出るパターン）]-----
+$targetText = "ABCDEABCDE";
+// 以下、どちらも置換内容は同一。（「A→B」「B→C」）
+$s3 = strtr($targetText, array('A' => 'B', 'B' => 'C'));           //strtr
+$s4 = str_replace(array('A', 'B'), array('B', 'C'), $targetText);  //str_replace
 
+echo $s3 . PHP_EOL;  //=> "BCCDEBCCDE"
+echo $s4 . PHP_EOL;  //=> "CCCDECCCDE"
+
+// ・置換１： A→B
+// ・置換２： B→C
+//
+// strtr は、『変換前の文字列』に対してのみ置換を行っている。
+// 「置換１： A→B」で "B" に置換された部分があったとしても、「置換２： B→C」の対象 B は対象外となる。
+//
+// str_replace は、「置換１： A→B」で "B" に変換された部分も、「置換２： B→C」の置換元の対象に含まれる。
+
+```
--- zzz_sample.php
+++ zzz_sample.php
@@ -1,16 +1,70 @@
 <?php
 
-class SampleClass01
-{
+echo "=============================================" . PHP_EOL;
 
-}
-$target_class = new SampleClass01();
+$targetText = "ABCDEABCDE";
+$s3 = strtr($targetText, array('A' => 'B', 'B' => 'C'));           //strtr
+$s4 = str_replace(array('A', 'B'), array('B', 'C'), $targetText);  //str_replace
 
-$target_class_name = get_class($target_class);
-echo "{$target_class_name}" . PHP_EOL; //=> SampleClass01
+echo $s3 . PHP_EOL;  //=> "BBEDEBBEDE"
+echo $s4 . PHP_EOL;  //=> "CDCDECDCDE"
 
-//-----( クラスから )-----
-$target_class_name = strval(SampleClass01::class);
-echo "{$target_class_name}" . PHP_EOL; //=> SampleClass01
+
+
+echo "=============================================" . PHP_EOL;
+
+$targetText = "110100";
+$s3 = strtr($targetText, array('1' => '0', '0' => '1'));           //strtr
+$s4 = str_replace(array('1', '0'), array('0', '1'), $targetText);  //str_replace
+
+echo $s3 . PHP_EOL;  //=> "001011"
+echo $s4 . PHP_EOL;  //=> "111111"
+
+
+
+
+
+// $targetText = "ABCABC";
+// $replaceText = str_replace("A", "×", $targetText);
+// echo $replaceText . PHP_EOL;  //=> "AbcdE"
+
+$targetText = "ABCABC";
+$replaceText = strtr($targetText, "A", "x");
+echo $replaceText . PHP_EOL;  //=> "xBCxBC"
+
+
+echo "=========================================" . PHP_EOL;  //=> "AbcdE"
+
+// strtr — 文字の変換あるいは部分文字列の置換を行う
+
+
+
+$s1 = strtr('abcde', array('a' => 'A', 'e' => 'E'));           //strtr
+$s2 = str_replace(array('a', 'e'), array('A', 'E'), 'abcde');  //str_replace
+
+echo $s1 . PHP_EOL;  //=> "AbcdE"
+echo $s2 . PHP_EOL;  //=> "AbcdE"
+
+
+$s3 = strtr('110100', array('1' => '0', '0' => '1'));           //strtr
+$s4 = str_replace(array('1', '0'), array('0', '1'), '110100');  //str_replace
+
+echo $s3 . PHP_EOL;  //=> "001011"
+echo $s4 . PHP_EOL;  //=> "111111"
+
+
+
+// class SampleClass01
+// {
+
+// }
+// $target_class = new SampleClass01();
+
+// $target_class_name = get_class($target_class);
+// echo "{$target_class_name}" . PHP_EOL; //=> SampleClass01
+
+// //-----( クラスから )-----
+// $target_class_name = strval(SampleClass01::class);
+// echo "{$target_class_name}" . PHP_EOL; //=> SampleClass01
 
 
