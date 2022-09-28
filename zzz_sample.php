da2ed27da07a7da27df94020903b961425d284bd
kakisoft <kakisttab@gmail.com>
2022-09-28 15:30:30 +0900

    a

--- zzz_sample.php
+++ zzz_sample.php
@@ -1,25 +1,11 @@
 <?php
 
-$original_json_data = '{"id":1, "product_name":"\u8a31\u53ef\u3055\u308c\u3066\u3044\u306a\u3044\u65b9\u6cd5"}';
-$decoded_data = json_decode($original_json_data, true);  // true の場合、返されるオブジェクトは連想配列形式。 false の場合、返されるオブジェクトは object。省略時は null（そのため false）
-
-// echo $decoded_data;
-// var_dump($decoded_data);
-print_r($decoded_data);
-
-// //==========================
-// //      URLエンコード
-// //==========================
-// //URLとして使用できない文字や記号を、使用できる文字の特殊な組み合わせで表すように変換する。
-
-// //----------( urlencode )----------
-// $a1 = urlencode('abc_defああああ');  //=>  abc_def%E3%81%82%E3%81%82%E3%81%82%E3%81%82
-// $result_1 = '<a href="nextpage?foo=' . urlencode($a1) . '">';  //=> <a href="nextpage?foo=abc_def%25E3%2581%2582%25E3%2581%2582%25E3%2581%2582%25E3%2581%2582">
+$a1 = 'https://eikaiwa.dmm.com/list/?data%5Btab1%5D%5Bstart_time%5D=02%3A00&data%5Btab1%5D%5Bend_time%5D=25%3A30&data%5Btab1%5D%5Bover_3years_experience%5D=0&data%5Btab1%5D%5Bcountry%5D=40%2C104%2C78%2C64%2C65%2C112%2C118%2C177%2C5%2C91%2C139%2C102%2C72%2C175%2C173%2C49%2C56%2C13%2C43%2C41%2C39%2C28%2C35%2C211%2C70%2C24%2C16%2C21%2C53%2C4%2C22%2C32%2C27%2C85%2C97%2C52%2C42%2C3%2C14%2C31%2C12%2C93%2C109%2C103%2C94%2C101%2C86%2C38%2C214%2C99%2C36%2C83%2C79%2C84%2C124%2C132%2C88%2C29%2C10%2C106%2C98%2C77%2C158%2C140%2C37%2C183%2C133%2C204%2C18%2C209%2C120%2C137%2C63%2C134%2C123%2C100%2C44%2C131%2C47%2C62%2C157%2C6%2C117%2C57%2C81%2C197%2C192%2C128%2C23%2C17%2C60%2C129%2C59%2C166%2C111%2C174%2C115%2C116%2C113%2C121&data%5Btab1%5D%5Bgender%5D=0&data%5Btab1%5D%5Bage%5D=%E5%B9%B4%E9%BD%A2&data%5Btab1%5D%5Bfree_word%5D=&data%5Btab1%5D%5Bnew%5D=0&data%5Btab1%5D%5Blesson_language%5D=en&date=2022-09-28&page=1';
+echo urldecode($a1);
 
 
-// //----------( rawurlencode )----------
-// // RFC 3986 に基づき URL エンコードを行う。urlencode関数よりrawurlencode関数を使ったほうが安全らしい。
-// $a2 = rawurlencode('abc_defああああ');  //=> abc_def%E3%81%82%E3%81%82%E3%81%82%E3%81%82
+
+
 
 // //==========================
 // //      URLデコード
